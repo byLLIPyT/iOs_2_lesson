cebac164a622da7e720683764b180b30d506b684
byLLIPyT <byllipyt@icloud.com>
2020-12-18 00:15:35 +0300

    Initial Commit

---
+++ iOs_2_lesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOs_2_lesson
+//
+//  Created by Александр Уткин on 18.12.2020.
+//  Copyright © 2020 Александр Уткин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
